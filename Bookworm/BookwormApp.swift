1df00cdc3641c8ea1f7afe75c78ad14d95a5a97e
CodeByDzyne <wdenley@dzynesolutions.com>
2022-12-17 16:25:06 +1100

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Wealthy Waz on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
